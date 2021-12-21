3e696a3ff4714ba6ee11b7eb7638310bd94e350f
wesley_wisch <wesley_wisch@hotmail.com>
2021-12-21 08:06:42 -0300

    Sidebar pronto

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Sidebar } from "./components/Sidebar";
 import { Navbar } from './components/Navbar';
 
 export function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { 
+  SidebarContainer, 
+  Icon, 
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from './styles';
+
+export function Sidebar() {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+        </SidebarMenu>
+
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+}
